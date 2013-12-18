e11437bef890fd93685136096c9ab2838b6f4526
Ward Muylaert <ward.muylaert@gmail.com>
2013-12-18 21:34:19 +0100

    Towards Heist templating
    Rewrite some stuff based on the Snap tutorial from
    http://snapframework.com/docs/tutorials/snaplets-tutorial

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,41 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
 
 import           Control.Applicative
+import           Control.Lens.TH
+import           Snap
 import           Snap.Core
 import           Snap.Util.FileServe
 import           Snap.Http.Server
 import           Snap.Snaplet.Heist
 
+data App = App
+    { _heist :: Snaplet (Heist App)
+    }
+
+makeLenses ''App
+
+instance HasHeist App where heistLens = subSnaplet heist
+
+appInit :: SnapletInit App App
+appInit = makeSnaplet "halendar" "The Haskell Calendar" Nothing $ do
+    hs <- nestSnaplet "heist" heist $ heistInit "templates"
+    addRoutes [
+        ("/signup", writeText "Signup page"),
+        ("/signin", writeText "Signin page"),
+        ("/navigate", writeText "Navigates the calendar"),
+        ("/publish", writeText "Publish an event"),
+        ("/delete", writeText "Delete an event")
+        ]
+    wrapSite (<|> heistServe)
+    return $ App hs
+
 main :: IO ()
-main = quickHttpServe site
+main = serveSnaplet defaultConfig appInit
+
+--main :: IO ()
+--main = quickHttpServe site
 
 site :: Snap ()
 site =
