34e4745082d280d030d8c0d7086bc8cff2fb8beb
Ward Muylaert <ward.muylaert@gmail.com>
2013-12-20 21:21:01 +0100

    Final changes of json->sqlite switch

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@
 module Main where
 
 import           Control.Applicative
+import           Control.Concurrent
 import           Control.Lens.TH
 import           Data.ByteString (ByteString)
 import           Snap
@@ -11,9 +12,10 @@ import           Snap.Util.FileServe
 import           Snap.Http.Server
 import           Snap.Snaplet.Heist
 import           Snap.Snaplet.Auth
-import           Snap.Snaplet.Auth.Backends.JsonFile
+import           Snap.Snaplet.Auth.Backends.SqliteSimple
 import           Snap.Snaplet.Session
 import           Snap.Snaplet.Session.Backends.CookieSession
+import           Snap.Snaplet.SqliteSimple
 ----
 import           Application
 import           Db
@@ -34,12 +36,21 @@ appInit :: SnapletInit App App
 appInit = makeSnaplet "halendar" "The Haskell Calendar" Nothing $ do
     h <- nestSnaplet "heist" heist $ heistInit "templates"
     s <- nestSnaplet "sess" sess $ initCookieSessionManager "site_key.txt" "sess" (Just 3600)
-    a <- nestSnaplet "auth" auth $ initJsonFileAuthManager defAuthSettings sess "users.json"
+
+    -- Initialize auth that's backed by an sqlite database
+    d <- nestSnaplet "db" db sqliteInit
+    a <- nestSnaplet "auth" auth $ initSqliteAuth sess d
+
+    -- Grab the DB connection pool from the sqlite snaplet and call
+    -- into the Model to create all the DB tables if necessary.
+    let c = sqliteConn $ d ^# snapletValue
+    liftIO $ withMVar c $ \conn -> Db.createTables conn
+
     addRoutes routes
     -- Add <ifLoggedIn> and <ifLoggedOut> tags in the heist templates
     addAuthSplices h auth
     wrapSite (<|> heistServe)
-    return $ App h s a
+    return $ App h s a d
 
 main :: IO ()
 main = serveSnaplet defaultConfig appInit
