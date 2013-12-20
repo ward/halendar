ac13d74d3dc39b63751a7f7b399d7408682652c6
Ward Muylaert <ward.muylaert@gmail.com>
2013-12-20 20:45:30 +0100

    +getEventsForUser, +saveEvent
    Some more DB work, hope it actually works...

--- src/Db.hs
+++ src/Db.hs
@@ -3,7 +3,7 @@ module Db (
   , Event(..)
   , createTables
   , saveEvent
-  , listEvents) where
+  , getEventsForUser) where
 
 import qualified Data.Text as T
 import           Data.Time (UTCTime)
@@ -23,7 +23,9 @@ data Event = Event
     , eventStart :: UTCTime
     , eventEnd :: UTCTime
     -- If we make something deriving from Enum, would it easily go in the database?
+    -- Could use fromEnum/toEnum?
     , eventRepeat :: Int
+    , eventOwner :: Int
     } deriving (Show)
 
 -- TODO: Need instance of fromRow most likely?
@@ -49,7 +51,7 @@ createTables conn = do
                       , "start TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
                       , "end TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
                       , "repeat INTEGER DEFAULT 0,"
-                      , "user_id INTEGER)" -- TODO: add SQLite foreign key constraint
+                      , "user_id INTEGER)" -- TODO: add SQLite foreign key constraint, need to know user table name for that
                       ])
     -- Given its a relation of user 1:many events, we can just put foreign key
     -- in the events database, no?
@@ -61,3 +63,18 @@ createTables conn = do
     --         T.concat [ "CREATE TABLE event_user ("
     --                  , 
     --                  ])
+
+getEventsForUser :: User -> Handler App Sqlite [Event]
+getEventsForUser (User user_id _) =
+    query "SELECT id, title, description, start, end, repeat, user_id FROM events WHERE user_id = ?" (Only user_id)
+
+saveEvent :: Event -> Handler App Sqlite ()
+saveEvent e = execute "INSERT INTO events (id, title, description, start, end, repeat, user_id) VALUES (?,?,?,?,?,?,?)"
+                      ( eventId e
+                      , eventTitle e
+                      , eventDescription e
+                      , eventStart e
+                      , eventEnd e
+                      , eventRepeat e
+                      , eventOwner e
+                      )
