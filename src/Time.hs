df4b31cb4b50af4b04cb99c50413b43ba620371e
Ward Muylaert <ward.muylaert@gmail.com>
2014-01-10 20:19:05 +0100

    Check, comment and finalize Time module

--- src/Time.hs
+++ src/Time.hs
@@ -1,6 +1,6 @@
 -- The repeating mentioned here includes skipping over a month that doesn't
 -- have the particular day we are following. Eg 31 January will have 31 March
--- next in the line.
+-- next in the line when repeating monthly.
 
 module Time (
     repeatDaily
@@ -27,9 +27,12 @@ sameDays :: (UTCTime, UTCTime) -> (UTCTime, UTCTime) -> Bool
 sameDays (start, end) (start', end') = sameDay start start' && sameDay end end'
 sameDay :: UTCTime -> UTCTime -> Bool
 sameDay (UTCTime d1 _) (UTCTime d2 _) = sameLast3 (toGregorian d1) (toGregorian d2)
-sameLast3 :: Eq c => (a, b, c) -> (a, b, c) -> Bool
-sameLast3 (_, _, x3) (_, _, y3) = x3 == y3
+    where
+        sameLast3 :: Eq c => (a, b, c) -> (a, b, c) -> Bool
+        sameLast3 (_, _, x3) (_, _, y3) = x3 == y3
 
+-- Indiscriminately starts repeating a date, the mentioned filtering is done
+-- through sameDays.
 repeatRange :: (Integer -> UTCTime -> UTCTime) -> (UTCTime, UTCTime) -> [(UTCTime, UTCTime)]
 repeatRange f range = zipWith fTuple [0..] $ repeat range
     where
@@ -40,7 +43,7 @@ addDay :: Integer -> UTCTime -> UTCTime
 addDay i (UTCTime day time) = UTCTime (addDays i day) time
 
 -- The clipping indicates the same clipping as in Data.Time.Calendar
--- eg: 31 march + 1 month = 30 april
+-- eg: 31 March + 1 month = 30 April
 addMonthClip :: Integer -> UTCTime -> UTCTime
 addMonthClip i (UTCTime day time) = UTCTime (addGregorianMonthsClip i day) time
 
