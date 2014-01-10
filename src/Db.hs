0dc0b19ab10362ee1662837f409c412292a24e3a
Ward Muylaert <ward.muylaert@gmail.com>
2014-01-10 21:34:47 +0100

    Make Repeat a proper type

--- src/Db.hs
+++ src/Db.hs
@@ -17,6 +17,10 @@ import           Data.List
 import qualified Data.Text as T
 import           Data.Time (UTCTime)
 import qualified Database.SQLite.Simple as S
+import           Database.SQLite.Simple.FromField
+import           Database.SQLite.Simple.ToField
+import           Database.SQLite.Simple.Internal
+import           Database.SQLite.Simple.Ok
 import           Text.Read
 import           Snap.Snaplet
 import           Snap.Snaplet.SqliteSimple
@@ -31,6 +35,18 @@ import           Time
 -- User ID login
 data User = User Int T.Text deriving (Show)
 
+data Repeat = Never | Daily | Monthly | Yearly
+    deriving (Show, Read, Enum)
+-- Need these to also be able to take it from/put it in the database
+-- Note the graceful failure if the value in the database is not an integer
+-- If it is an integer but doesn't fit in our Enum, then hissyfits will be thrown
+instance FromField Repeat where
+    fromField (Field (S.SQLInteger i) _) = Ok (toEnum . fromIntegral $ i :: Repeat)
+    fromField _ = Ok Never
+instance ToField Repeat where
+    toField = S.SQLInteger . fromIntegral . fromEnum
+
+
 data Event = Event
     { eventId :: Int
     , eventTitle :: T.Text
@@ -39,11 +55,10 @@ data Event = Event
     , eventEnd :: UTCTime
     -- If we make something deriving from Enum, would it easily go in the database?
     -- Could use fromEnum/toEnum?
-    , eventRepeat :: Int
+    , eventRepeat :: Repeat
     , eventOwner :: Int
     } deriving (Show)
-
--- TODO: Can't this be written more concisely?
+-- For pulling from the database
 instance FromRow Event where
     fromRow = Event <$> field <*> field <*> field <*> field <*> field <*> field <*> field
 
@@ -57,10 +72,10 @@ repeatEvent e = map timesToEvent $ getRepeater (eventStart e, eventEnd e)
         timesToEvent (start, end) = e { eventStart = start, eventEnd = end }
         getRepeater :: (UTCTime, UTCTime) -> [(UTCTime, UTCTime)]
         getRepeater = case eventRepeat e of
-            3 -> repeatYearly
-            2 -> repeatMonthly
-            1 -> repeatDaily
-            _ -> \(es, ee) -> [(es, ee)]
+            Yearly -> repeatYearly
+            Monthly -> repeatMonthly
+            Daily -> repeatDaily
+            Never -> \(es, ee) -> [(es, ee)]
 
 eventTimeOrder :: Event -> Event -> Ordering
 eventTimeOrder e1 e2 = case eventStart e1 `compare` eventStart e2 of
